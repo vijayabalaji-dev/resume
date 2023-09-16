18d14b16345be42791d9141df8a44e2c06387a19
Vijayabalaji.J <jvijayabalaji03082002@gmail.com>
2023-09-16 16:12:23 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ResumeComponent } from './resume/resume.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ResumeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
